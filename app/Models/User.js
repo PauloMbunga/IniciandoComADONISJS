36265b64fb06653539145c08655f8c98a6fda46f
Paulo Mbunga <paulombunga@gmail.com>
2020-05-23 16:07:38 +0100

    Adicionando métodos index,show,store,destroy

--- app/Models/User.js
+++ app/Models/User.js
@@ -35,6 +35,11 @@ class User extends Model {
   tokens () {
     return this.hasMany('App/Models/Token')
   }
+
+  tweets(){
+
+    return this.hasMany('App/Models/Tweet');
+  }
 }
 
 module.exports = User
--- start/routes.js
+++ start/routes.js
@@ -19,3 +19,11 @@ const Route = use('Route')
 Route.post("/register","AuthController.register");
 Route.post("/authenticate","AuthController.authenticate");
 Route.get("/app","AppController.index").middleware(["auth"]);
+
+Route.group(()=> {
+
+  Route.resource("tweets","TweetController")
+  .apiOnly()
+  .except('update');
+
+}).middleware('auth');
